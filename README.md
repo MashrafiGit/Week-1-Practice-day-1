1a374abb4e01e23774fda2d77d3d72d1b2fa73ce
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-10 15:19:41 +0600

    zero or non zero

---
+++ README.md
@@ -0,0 +1,7 @@
+## #Week 1 - Practice day 1
+
+1. Zero or Non Zero
+2. 5 Marks for good hand writing
+3. Multiple or not
+4. Floating Point Number
+
---
+++ zero_nonZero.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    // get value from user
+    int num;
+    scanf("%d\n", &num);
+
+    
+    if(num==0){
+        printf("Zero");
+    }
+    else{
+        printf("Non Zero");
+    }
+
+    return 0;
+}
